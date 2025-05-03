7d0f693d4105fb12e7ac3aa75415c32619576072
Kezia Sharnoff <105600498+ksharnoff@users.noreply.github.com>
2025-05-03 00:44:31 -0500

    Popup gets new button to manually trigger a current blocking update
    Done so with alarms that background js will hear. Also, removed button
    to block all sites from popup html,, may add it back later but will
    have to add more sophisticated alarm system.

--- popup.js
+++ popup.js
@@ -1,10 +1,26 @@
-// Listener to settings button, if pressed creates new tab of settings.html
+// Creates new tab of settings.html
 document.getElementById("settings").addEventListener("click", function() {
 	chrome.tabs.create({ url: chrome.runtime.getURL("../settings.html")});
 });
 
-// Listener to help button, if pressed creates new tab of help.html for 
-// help inforamtion
+
+// Creates new tab of help.html 
 document.getElementById("help").addEventListener("click", function() {
 	chrome.tabs.create({ url: chrome.runtime.getURL("../help.html")});
 });
+
+
+// If click the "I changed timezones / recheck" button then set alarm to go
+// off that background.js will hear and re-calculate who should be blocked
+// now! 
+// The alarm must have a minimum 30 seconds wait because chrome says so
+
+let recheckButton = document.getElementById("recheck");
+
+recheckButton.addEventListener("click", function() {
+	recheckButton.innerHTML = "the button worked! wait 30 seconds";
+	console.log("popup demands manual recheck!");
+	chrome.alarms.create("updateBlockingAlarm", { 
+		delayInMinutes: 0.5
+	});
+})
