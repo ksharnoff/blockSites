cf68427dadb72d3b13d397960d270f6ae425f5e4
Kezia Sharnoff <105600498+ksharnoff@users.noreply.github.com>
2025-06-14 23:47:25 -0700

    Initial commit CC license for photos, added copyright info to blocked page

--- blocked.js
+++ blocked.js
@@ -29,13 +29,13 @@ const images = [
 	"./images/tree.jpeg"
 	];
 const altTexts = [
-	"two red circular fireworks errupting in black sky",
+	"two red circular fireworks erupting in black sky",
 	"strands of grass level and close to the camera, bigger cacti are out of focus in the background",
 	// "alt text here about your image1",
 	// "alt text here about your image2",
 	// "alt text here about your image3",
 	"gray and brown squirrel looks with one eye towards the camera, the background is blurred",
-	"geyser errupting, the base is white rock, there are short trees in the distance, the sky is blue",
+	"geyser erupting, the base is white rock, there are short trees in the distance, the sky is blue",
 	"bee on a pink petal on gray stone on the ground, with blurred green plants in the background", 
 	"green and orange tree leaves in front of blue sky with few clouds"
 	];
@@ -48,4 +48,26 @@ window.addEventListener("load", function() {
 	// the alt text array has the same index as the photo in the images array
 	randImage.src = images[rand];
 	randImage.alt = altTexts[rand];
+
+
+	let copyrightElement = document.getElementById("copyright");
+
+	// set the copyright text if the image is one of Kezia's
+	switch (images[rand]) {
+	case "./images/fireworks.jpeg":
+	case "./images/grass.jpeg":
+	case "./images/squirrel.png":
+	case "./images/geyser.jpeg":
+	case "./images/bee.jpeg":
+	case "./images/tree.jpeg":
+		let copyright = "All images are licensed under ";
+		copyright += "<a href = 'https://creativecommons.org/licenses/by-nc-sa/4.0/'> CC BY-NC-SA 4.0 </a>";
+		copyright += "<br> Any usage of these images must provide attribution in the form of a link to the "; 
+		copyright += "<a href = 'https://github.com/ksharnoff/blockSites'> blockSites Github repo</a>"
+		copyright += " where \"usage\" includes the availability of any generative model trained on them.";
+		copyrightElement.innerHTML = copyright;
+		break;
+	default:
+		break;
+	} 
 })
